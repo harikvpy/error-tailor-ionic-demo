478019095a2296ace4fec1d323ab7cc2d7f6b8ec
Hari Mahadevan <hari@smallpearl.com>
2023-05-25 10:00:00 +0800

    Change error control to use <ion-note  slot='error'..>

--- src/app/ion-control-error/ion-control-error.component.ts
+++ src/app/ion-control-error/ion-control-error.component.ts
@@ -1,14 +1,16 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef } from '@angular/core';
 import { DefaultControlErrorComponent } from '@ngneat/error-tailor';
 
+/**
+ * All our form controls are laid out within an ion-item which in turn
+ * are within a parent ion-list. This custom error control displays
+ * the error, using the new ion-note[slot='error'] Ionic component.
+ */
 @Component({
   selector: 'custom-control-error',
   template: `
-    <ion-item lines="none" class="ion-text-wrap" [class.hide-control]="hideError">
-      <ion-label color="danger" class="ion-no-margin ion-text-wrap" stacked>
-        {{ errorText }}
-      </ion-label>
-    </ion-item>
+    <ion-note slot="error" color='danger'>{{ errorText }}</ion-note>
+    <ng-template *ngTemplateOutlet="errorTemplate; context: errorContext"></ng-template>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   styles: [
@@ -34,11 +36,21 @@ export class IonControlErrorComponent extends DefaultControlErrorComponent {
  * Uses direct manipulation of DOM.
  */
 export function anchorIonErrorComponent(hostElement: Element, errorElement: Element) {
-  hostElement.parentElement.insertAdjacentElement('afterend', errorElement);
+  const notes = errorElement.getElementsByTagName('ion-note');
+  if (notes.length > 0) {
+    const noteElem = notes[0];
+    errorElement.removeChild(noteElem);
+    // console.log('anchorIonErrorComponent - parentElement:', hostElement.parentElement)
+    hostElement.parentElement.appendChild(noteElem);
+  }
   return () => {
-    let errorNode = hostElement.parentElement.querySelector('custom-control-error');
-    if (errorNode) {
-      errorNode.remove();
+    const ionNoteNode = hostElement.parentElement.querySelector('ion-note[slot="error"]');
+    if (ionNoteNode) {
+      ionNoteNode.remove();
+    }
+    const customErrorControlNode = hostElement.parentElement.querySelector('custom-control-error');
+    if (customErrorControlNode) {
+      customErrorControlNode.remove();
     }
   };
 }
--- src/app/ion-control-error/ion-control-error.module.ts
+++ src/app/ion-control-error/ion-control-error.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonControlErrorComponent } from './ion-control-error.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [IonControlErrorComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule],
   exports: [IonControlErrorComponent]
 })
 export class IonControlErrorComponentModule {}
