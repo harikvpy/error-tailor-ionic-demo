e2f61ec1d29c0df1e48d6e7065a85ea242b30caa
Hari Mahadevan <hari@smallpearl.com>
2023-05-25 09:53:13 +0800

    Upgrade ng@13 & @ionic/angular@6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,36 +13,35 @@ import { IonControlErrorComponentModule } from './ion-control-error/ion-control-
 import { IonControlErrorComponent, anchorIonErrorComponent } from './ion-control-error/ion-control-error.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    IonControlErrorComponentModule,
-    ErrorTailorModule.forRoot({
-      errors: {
-        useFactory() {
-          return {
-            required: 'This field is required',
-            email: 'Invalid email address',
-            minlength: ({ requiredLength, actualLength }) => `Expect ${requiredLength} but got ${actualLength}`,
-          };
-        },
-        deps: []
-      },
-      blurPredicate: (element: Element) => {
-        return element.tagName === 'ION-INPUT' || element.tagName === 'ION-SELECT';
-      },
-      controlErrorComponent: IonControlErrorComponent,
-      controlErrorComponentAnchorFn: anchorIonErrorComponent
-    })
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        IonControlErrorComponentModule,
+        ErrorTailorModule.forRoot({
+            errors: {
+                useFactory() {
+                    return {
+                        required: 'This field is required',
+                        email: 'Invalid email address',
+                        minlength: ({ requiredLength, actualLength }) => `Expect ${requiredLength} but got ${actualLength}`,
+                    };
+                },
+                deps: []
+            },
+            blurPredicate: (element: Element) => {
+                return element.tagName === 'ION-INPUT' || element.tagName === 'ION-SELECT';
+            },
+            controlErrorComponent: IonControlErrorComponent,
+            controlErrorComponentAnchorFn: anchorIonErrorComponent
+        })
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
